6c377a6dbae975dae2b312b41eba98fa07ef1411
aldo-04 <ordenaldo@gmail.com>
2021-12-11 13:14:48 -0300

    enlazando componentes

---
+++ src/components/Error.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Error(){
+    return (
+        <React.Fragment>
+            {/*<!-- Content Wrapper -->*/}
+            <div id="content-wrapper" className="d-flex flex-column">
+                {/*<!-- Main Content -->*/}
+                <div id="content">
+                    <h1> Pagina de error </h1>
+                </div>
+            </div>    
+        </React.Fragment>
+    )
+}
+export default Error;
